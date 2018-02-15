17424fa997a656ce09e384fa5db69ceb0aab379c
chris-daynes <chrisdaynes@gmail.com>
2018-02-16 11:49:13 +1300

    added bcrypt to hash the password

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 
-userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
   username: {type: String, unique: true},
   fullname: {type: String, unique: true, default: ''},
   email: {type: String, unique: true},
@@ -12,4 +13,14 @@ userSchema = mongoose.Schema({
   googleTokens: Array
 })
 
-module.exports = mongoose.model(userSchema)
+userSchema.methods.encryptPassword = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+};
+
+userSchema.methods.validUserPassword = function(password) {
+  return bcrypt.compareSync(password, this.password)
+};
+
+
+
+module.exports = mongoose.model('User', userSchema)
