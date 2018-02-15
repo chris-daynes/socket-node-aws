55f8daa2035dd116c2d0d151a7832ab8cf2ea9be
chris-daynes <chrisdaynes@gmail.com>
2018-02-16 12:22:30 +1300

    signup working with passport and bcrypt. Storing in mongodb/localhost/udemy/users

--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,27 @@
 'use strict';
 
-module.exports = function (_) {
+module.exports = function (_, passport) {
 
   return {
     SetRouting: function(router) {
       router.get('/', this.indexPage);
       router.get('/signup', this.getSignUp);
+      router.get('/home', this.homePage);
+
+
+      router.post('/signup', this.postSignup)
     }, 
 
-    indexPage: (req, res) => {
-      return res.render('index', {test: 'This is a testttttt'});
-    },
+    indexPage: (req, res) => res.render('index', {test: 'This is a testttttt'}),
 
-    getSignUp: (req, res) => {
-      return res.render('signup');
-    }
+    getSignUp: (req, res) => res.render('signup'),
+
+    postSignup: passport.authenticate('local.signup', {
+      successRedirect: '/home',
+      failureRedirect: '/signup',
+      failureFlash: true
+    }),
+
+    homePage: (req, res) => res.render('home')
   }
 }
