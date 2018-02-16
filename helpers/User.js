64fc0110b184bec2118b373d6e6216053483fc14
chris-daynes <chrisdaynes@gmail.com>
2018-02-16 21:15:54 +1300

    added validation for the signup route in /helpers/User.js

---
+++ helpers/User.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function() {
+  return {
+    SignUpValidation: (req, res, next) => {
+      req.checkBody('username', 'Username is Required').notEmpty();
+      req.checkBody('username', 'Username must be more than 5 characters').isLength({min: 5});
+      req.checkBody('email', 'Email is required').notEmpty()
+      req.checkBody('email', 'Email is invalid').isEmail({})
+      req.checkBody('password', 'Password is required').notEmpty();
+      req.checkBody('password', 'Must be greater than 5 characters').isLength({min: 5});
+    }
+  }
+}
